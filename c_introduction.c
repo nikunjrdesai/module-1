ad1191942cd2d639ea97ba093e6b9ec0d8624c08
nikunjrdesai <desainikunj269@gmail.com>
2023-05-02 10:32:22 +0530

    module 1 complate

---
+++ c_introduction.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+
+printf(" my name is nikunj rasikbhai desai\n my birthdate is 26 sep,1998\n i am 24 years old\n my home address is a/69,sargam society,punagam.");
+
+}
